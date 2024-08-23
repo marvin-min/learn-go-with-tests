5bb70d312d5d6f408ae81f12cfd8f3db040b8f5f
marvin-min <xiaorong.min@gmail.com>
2024-08-23 11:33:50 +0800

    configurable

--- select_demo/racer.go
+++ select_demo/racer.go
@@ -1,21 +1,47 @@
 package select_demo
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(a, b string) (winner string) {
-	startA := time.Now()
-	http.Get(a)
-	aDuration := time.Since(startA)
-
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
-
+func OldRacer(a, b string) (winner string) {
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
 	if aDuration < bDuration {
 		return a
 	}
 	return b
+}
+
+var tenSecondTimeout = 10 * time.Second
+
+func Racer(a, b string) (winner string, err error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, err error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	duration := time.Since(start)
+	return duration
 }
--- select_demo/racer_test.go
+++ select_demo/racer_test.go
@@ -1,15 +1,54 @@
 package select_demo
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.dev"
+	// t.Run("compares speeds of servers, returning the url of the fastest one", func(t *testing.T) {
+	// 	slowURL := "http://www.facebook.com"
+	// 	fastURL := "http://www.quii.dev"
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+	// 	want := fastURL
+	// 	got := Racer(slowURL, fastURL)
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+	// 	if got != want {
+	// 		t.Errorf("got %q, want %q", got, want)
+	// 	}
+	// })
+
+	t.Run("compares speeds of servers, returning the url of the fastest one", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
+		defer slowServer.Close()
+		defer fastServer.Close()
+		slowUrl := slowServer.URL
+		fastUrl := fastServer.URL
+		want := fastUrl
+		got, _ := Racer(slowUrl, fastUrl)
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
+
+		defer server.Close()
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+	return fastServer
 }
