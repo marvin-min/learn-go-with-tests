aba576fc128a7bd01ae06b5fd0a44e03e6f8a4e1
Marvin.Min <xiaorong.min@gmail.com>
2024-09-16 19:54:33 +0800

    demo

--- read_files/blog_post_test.go
+++ read_files/blog_post_test.go
@@ -1,20 +1,47 @@
 package read_files
 
 import (
+	"errors"
+	"io/fs"
+	"reflect"
 	"testing"
 	"testing/fstest"
 )
 
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh no, i always fail")
+}
 func TestNewBlogPosts(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1`
+		secondBody = `Title: Post 2
+Description: Description 2`
+	)
 	fs := fstest.MapFS{
-		"hello_world.md":  {Data: []byte("hei")},
-		"hello_world2.md": {Data: []byte("hola")},
+		"hello_world.md":  {Data: []byte(firstBody)},
+		"hello_world2.md": {Data: []byte(secondBody)},
 	}
 
-	posts := NewPostFromFS(fs)
+	posts, err := NewPostFromFS(fs)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := posts[0]
+	want := Post{Title: "Post 1", Description: "Description 1"}
 
+	assetPost(got, want, t)
 	if len(posts) != len(fs) {
 		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
 	}
 }
-https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/reading-files
+
+func assetPost(got, want Post, t *testing.T) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
--- read_files/post.go
+++ read_files/post.go
@@ -1,20 +1,47 @@
 package read_files
 
 import (
+	"io"
 	"io/fs"
 )
 
 type Post struct {
-	Title, Description, Body string
-	Tags                     []string
+	Title, Description string
+	// , Description, Body string
+	// Tags                     []string
 }
 
-func NewPostFromFS(fileSystem fs.FS) []Post {
-	dir, _ := fs.ReadDir(fileSystem, ".")
+func NewPostFromFS(fileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".")
+	if err != nil {
+		return nil, err
+	}
 	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f.Name())
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
 	}
 
-	return posts
+	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+
+	return newPost(postFile)
+}
+func newPost(postFile io.Reader) (Post, error) {
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+	post := Post{Title: string(postData)[7:]}
+	return post, nil
 }
