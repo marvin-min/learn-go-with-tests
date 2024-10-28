57ac16a1fc3131c1483b68039e48a205156e2b2d
Marvin.Min <xiaorong.min@gmail.com>
2024-10-28 23:03:27 +0800

    add more

--- http_server/file_system_store_test.go
+++ http_server/file_system_store_test.go
@@ -17,5 +17,24 @@ func TestFileSystemStore(t *testing.T) {
 			{"Chris", 33},
 		}
 		assertLeague(got, want, t)
+		got = store.GetLeague()
+		assertLeague(got, want, t)
 	})
+
+	t.Run("get player score", func(t *testing.T) {
+		database := strings.NewReader(`[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}]`)
+		store := FileSystemPlayerStore{database}
+		got := store.GetPlayerScore("Chris")
+		want := 33
+		assertScoreEquals(got, want, t)
+	})
+}
+
+func assertScoreEquals(got int, want int, t *testing.T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
 }
