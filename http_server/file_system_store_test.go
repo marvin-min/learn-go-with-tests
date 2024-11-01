d80c894dcaee07c7b87e8e1f244ebfec88ac0308
Marvin.Min <xiaorong.min@gmail.com>
2024-11-01 22:32:59 +0800

    first commit

--- http_server/file_system_store_test.go
+++ http_server/file_system_store_test.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"strings"
+	"io"
+	"os"
 	"testing"
 )
 
 func TestFileSystemStore(t *testing.T) {
 	t.Run("league from a reader", func(t *testing.T) {
-		database := strings.NewReader(`[
+		database, cleanDatabase := createTempFile(t, `[
 			{"Name": "Cleo", "Wins": 10},
 			{"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
+
 		store := FileSystemPlayerStore{database}
 		got := store.GetLeague()
 		want := []Player{
@@ -22,12 +25,28 @@ func TestFileSystemStore(t *testing.T) {
 	})
 
 	t.Run("get player score", func(t *testing.T) {
-		database := strings.NewReader(`[
+		database, cleanDatabase := createTempFile(t, `[
 			{"Name": "Cleo", "Wins": 10},
 			{"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
 		store := FileSystemPlayerStore{database}
 		got := store.GetPlayerScore("Chris")
 		want := 33
+		assertScoreEquals(got, want, t)
+	})
+
+	t.Run("store wins for existing players", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, `[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
+
+		store := FileSystemPlayerStore{database}
+
+		store.RecordWin("Chris")
+
+		got := store.GetPlayerScore("Chris")
+		want := 34
 		assertScoreEquals(got, want, t)
 	})
 }
@@ -38,4 +57,19 @@ func assertScoreEquals(got int, want int, t *testing.T) {
 		t.Errorf("got %d want %d", got, want)
 	}
 }
-https://quii.gitbook.io/learn-go-with-tests/build-an-application/io#write-the-test-first-2
+
+func createTempFile(t testing.TB, initialData string) (io.ReadWriteSeeker, func()) {
+	t.Helper()
+	tmpfile, err := os.CreateTemp("", "db")
+	if err != nil {
+		t.Fatalf("could not create temp file %v", err)
+	}
+	tmpfile.Write([]byte(initialData))
+
+	removeFile := func() {
+		tmpfile.Close()
+		os.Remove(tmpfile.Name())
+	}
+	return tmpfile, removeFile
+
+}
