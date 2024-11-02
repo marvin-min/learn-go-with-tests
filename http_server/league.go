b34092444f0273cc3103f7c27e665f1ede4f71af
Marvin.Min <xiaorong.min@gmail.com>
2024-11-02 22:10:16 +0800

    use league type

--- http_server/league.go
+++ http_server/league.go
@@ -6,11 +6,22 @@ import (
 	"io"
 )
 
-func NewLeague(reader io.Reader) ([]Player, error) {
-	var league []Player
+type League []Player
+
+func NewLeague(reader io.Reader) (League, error) {
+	var league League
 	err := json.NewDecoder(reader).Decode(&league)
 	if err != nil {
 		err = fmt.Errorf("problem parsing league, %v", err)
 	}
 	return league, err
+}
+
+func (l League) Find(name string) *Player {
+	for i, player := range l {
+		if player.Name == name {
+			return &l[i]
+		}
+	}
+	return nil
 }
